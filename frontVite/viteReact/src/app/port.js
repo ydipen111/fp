72999627b408b113850c8ce3aa797926ad82db17
Your Name <aggressivebanda74@gmail.com>
2024-12-10 09:51:25 +0545

    onrender

--- frontVite/viteReact/src/app/port.js
+++ frontVite/viteReact/src/app/port.js
@@ -1,4 +1,5 @@
-export const base = 'https://fp-z3nb.onrender.com'
+export const base = 'https://fp-z3nb.onrender.com '
 export const baseUrl = `${base}/api`
 // https://fp-z3nb.onrender.com - render
+
 // http://192.168.18.108:1999 --localhost
