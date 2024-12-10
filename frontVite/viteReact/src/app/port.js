1cf6c5259cede5956d05ef7cc2b1b1a71aef2cd3
Your Name <aggressivebanda74@gmail.com>
2024-12-10 07:23:25 +0545

    commiting it

--- frontVite/viteReact/src/app/port.js
+++ frontVite/viteReact/src/app/port.js
@@ -1,3 +1,4 @@
-export const base = 'http://192.168.18.108:1999'
+export const base = 'https://fp-z3nb.onrender.com'
 export const baseUrl = `${base}/api`
-// https://fp-z3nb.onrender.com - render  
+// https://fp-z3nb.onrender.com - render
+//http://192.168.18.108:1999 --localhost
